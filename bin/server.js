36957ed31558da5c386de28533474ac81644f2d1
geovana.souza <geovana.souza@bravi.com.br>
2016-11-17 10:05:54 -0200

    Add timeout to server

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,6 @@
 var app = require('..');
 
-
-app.listen(process.env.HTTP_PORT);
-
+var server = app.listen(process.env.HTTP_PORT);
+server.timeout = parseInt(process.env.APP_TIMEOUT, 10) || 12000;
 
 console.log('Server started');
